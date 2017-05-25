3921136c6927d2e6c99b3cf7ff86b180b915de63
kuroidoruido <anthony.pena@outlook.fr>
2017-05-25 22:26:54 +0200

    v0.0.1

---
+++ js/script.js
@@ -0,0 +1,88 @@
+function formatToString(input) {
+	var output = "";
+	if(input.length > 3) {
+		output += addFormat('',input[0],input[1]);
+		for(var i=1 ; i<input.length-1 ; i++) {
+			output += addFormat(input[i-1],input[i],input[i+1],output[output.length-1]);
+		}
+		output += addFormat(input[input.length-2],input[input.length-1],'',output[output.length-1]);
+	}
+	output = correctTextWithComma(output);
+	output = replaceSpecialChars(output);
+	return output;
+}
+
+var indent = "";
+
+function addFormat(previousChar,current,nextChar,lastOutputChar) {
+	var output = [];
+	
+	if(lastOutputChar === '\n' && !(current === ']' || current === '}' || /\s/.test(current))) {
+		output.push(indent);
+	}
+	
+	if(current === '[' || current === '{') {
+		output.push(current);
+		output.push('\n');
+		indent += "\t";
+	} else if(current === ']' || current === '}') {
+		indent = indent.substring(1);
+		output.push(indent);
+		output.push(current);
+		if(nextChar !== ',') {
+			output.push('\n');
+		}
+	} else if(current === ',' || (nextChar === ']' || nextChar === '}')) {
+		output.push(current);
+		output.push('\n');
+	} else if(/\s/.test(current) && previousChar === ',') {
+		output.push('');
+	} else {
+		output.push(current);
+	}
+
+	return output.join('');
+}
+
+function correctTextWithComma(str) {
+	var input = str.split("\n");
+	var output = "";
+	for(i in input) {
+		var line = input[i];
+		if(output.length > 1 
+		&& output[output.length-2] === ','
+		&& line.indexOf('=') === -1
+		&& line.indexOf('[') === -1 && line.indexOf(']') === -1
+		&& line.indexOf('{') === -1 && line.indexOf('}') === -1
+		) {
+			output = output.slice(0,-1);
+			line = line.trim();
+		}
+		output += line + "\n";
+	}
+	return output;
+}
+
+function replaceSpecialChars(str) {
+	return str.replace(/\t/g,'    ');
+}
+
+function isUpperCase(ch) {
+	if (!isNaN(ch * 1)) {
+        return false;
+    } else {
+        if (ch == ch.toUpperCase()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+var handler = (event) => {
+	var input = $('#input').val();
+	$('#output').text(
+		formatToString(
+			input));
+};
+
+$('#input').change(handler);
